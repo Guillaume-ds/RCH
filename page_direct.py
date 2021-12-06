e1b3994df2432c478843cff82baa28ad98b6dbaa
Guillaume <guillaumedesurville@gmail.com>
2021-12-06 11:31:27 +0100

    ssl

--- page_direct.py
+++ page_direct.py
@@ -7,4 +7,7 @@ def page_direct():
     link = st.text_input(label= "Link of the video")
     play = st.button("play the video")
     if play:
-        st_player(f"{link}")
+        st_player(f"{link}&parent=https://share.streamlit.io/guillaume-ds/rch/main.py")
+    
+    st.markdown("""<iframe src="https://www.twitch.tv/mauriennisezvous" />
+            """, unsafe_allow_html = True)

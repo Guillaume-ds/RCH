eb33fc8890cb3ddcf208bcc26163a008b6113f34
Guillaume <guillaumedesurville@gmail.com>
2021-12-06 11:42:22 +0100

    test twitch

--- page_direct.py
+++ page_direct.py
@@ -5,9 +5,10 @@ from streamlit_player import st_player
 def page_direct():
     
     link = st.text_input(label= "Link of the video")
+    link2 = link+"&parent=share.streamlit.io&parent=https://share.streamlit.io/guillaume-ds/rch/main.py&parent=share.streamlit.io/guillaume-ds/rch/main.py"
     play = st.button("play the video")
     if play:
-        st_player(f"{link}&parent=https://share.streamlit.io/guillaume-ds/rch/main.py")
-    
-    st.markdown("""<iframe src="https://www.twitch.tv/mauriennisezvous" />
-            """, unsafe_allow_html = True)
+        st_player(link2)
+        st.write(link2)
+        st_player(link)
+        st.write(link)

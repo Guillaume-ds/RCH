9a3e8ea28c34737c23e73bf8b436b75971cd139d
Guillaume <guillaumedesurville@gmail.com>
2021-12-08 11:39:50 +0100

    faible espoir

--- page_direct.py
+++ page_direct.py
@@ -5,10 +5,11 @@ from streamlit_player import st_player
 def page_direct():
     
     link = st.text_input(label= "Link of the video")
-    link2 = link+"&parent=rchendirect.herokuapp.com/&parent=share.streamlit.io/guillaume-ds/rch/main.py"
+    link2 = link+"&parent=https://share.streamlit.io/guillaume-ds/rch/main.py"
     play = st.button("play the video")
     if play:
         st_player(link2)
         st.write(link2)
         st_player(link)
         st.write(link)
+        st.video(link)

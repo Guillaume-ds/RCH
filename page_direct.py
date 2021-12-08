1ad39bb11f92c74c43915051546ab1855183616f
Guillaume <guillaumedesurville@gmail.com>
2021-12-08 16:58:58 +0100

    mj answer

--- page_direct.py
+++ page_direct.py
@@ -5,13 +5,5 @@ from streamlit_player import st_player
 
 def page_direct():
     
-    link = st.text_input(label= "Link of the video")
-    link2 = link+"&parent=share.streamlit.io&parent="
-    play = st.button("play the video")
-    if play:
-        st_player(link)
-        st.write(link)
-    st_player(link2)
-    st.write(link2)          
-    st.markdown("""<iframe src="https://www.twitch.tv/reservoirlive&amp;parent=localhost" height=" 600" width="900" allowfullscreen="true"></iframe>""",unsafe_allow_html=True)
-    st.markdown("""<iframe width="100%" height="100%" src="https://player.twitch.tv/?autoplay=false&channel=channel%3Dpronewstv&parent=share.streamlit.io&parent=" allow="accelerometer; autoplay; encrypted-media; picture-in-picture; fullscreen"  style="position: absolute; top: 0px; left: 0px; width: 100%; height: 100%;"></iframe>""",unsafe_allow_html=True)
+    st_player("https://www.twitch.tv/reservoirlive")          
+    st.markdown("""<iframe src="https://player.twitch.tv/?channel=reservoirlive&parent=share.streamlit.io&parent=localhost "></iframe>""",unsafe_allow_html=True)

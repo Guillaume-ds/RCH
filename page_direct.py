1ce7acbbc4d32c3e7865618c9b60a507d8c39830
Guillaume <guillaumedesurville@gmail.com>
2021-12-08 15:41:20 +0100

    test

--- page_direct.py
+++ page_direct.py
@@ -12,10 +12,22 @@ def page_direct():
     link2 = link+"&parent=share.streamlit.io/guillaume-ds/rch/main.py"
     play = st.button("play the video")
     if play:
-        st_player(link2)
-        st.write(link2)
         st_player(link)
         st.write(link)
-        st.video(link)
-        components.iframe(link)
-        components.iframe(link2)
+            
+    st.markdown("""
+                <script src= "https://player.twitch.tv/js/embed/v1.js"></script>
+                <div id="test"></div>
+                <script type="text/javascript">
+                    var options = {
+                        width: 600,
+                        height: 300,
+                        channel: "bikeman",
+                        parent: ["localhost"]
+                    };
+                    var player = new Twitch.Player("test", options);
+                </script>
+    """,unsafe_allow_html=True)
+    
+    st.markdown("""<iframe src="https://player.twitch.tv/?channel=pronewstv" height=" 600" width="900" allowfullscreen="true"></iframe>""",unsafe_allow_html=True)
+    

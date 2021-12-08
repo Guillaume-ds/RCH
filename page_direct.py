09c72f981a1f35583b4f63cc3f39be9e93b3ce65
Guillaume <guillaumedesurville@gmail.com>
2021-12-08 16:59:55 +0100

    ptept

--- page_direct.py
+++ page_direct.py
@@ -6,4 +6,4 @@ from streamlit_player import st_player
 def page_direct():
     
     st_player("https://www.twitch.tv/reservoirlive")          
-    st.markdown("""<iframe src="https://player.twitch.tv/?channel=reservoirlive&parent=share.streamlit.io&parent=localhost "></iframe>""",unsafe_allow_html=True)
+    st.markdown("""<iframe src="https://player.twitch.tv/?channel=reservoirlive&parent=share.streamlit.io"></iframe>""",unsafe_allow_html=True)

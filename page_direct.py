0ee54624d4e5313b11a2f9988cb0dc0a06662831
Guillaume <guillaumedesurville@gmail.com>
2021-12-08 11:47:27 +0100

    puse

--- page_direct.py
+++ page_direct.py
@@ -1,11 +1,15 @@
 import streamlit as st
+import streamlit.components.v1 as components
+
+# embed streamlit docs in a streamlit app
+
 
 from streamlit_player import st_player
 
 def page_direct():
     
     link = st.text_input(label= "Link of the video")
-    link2 = link+"&parent=https://share.streamlit.io/guillaume-ds/rch/main.py"
+    link2 = link+"&parent=share.streamlit.io/guillaume-ds/rch/main.py"
     play = st.button("play the video")
     if play:
         st_player(link2)
@@ -13,3 +17,5 @@ def page_direct():
         st_player(link)
         st.write(link)
         st.video(link)
+        components.iframe(link)
+        components.iframe(link2)

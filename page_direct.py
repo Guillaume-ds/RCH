b5827afc378d3b7cfe5b0b9f3e8290584d6e31f6
Guillaume <guillaumedesurville@gmail.com>
2021-12-08 15:46:33 +0100

    generator

--- page_direct.py
+++ page_direct.py
@@ -29,5 +29,5 @@ def page_direct():
                 </script>
     """,unsafe_allow_html=True)
     
-    st.markdown("""<iframe src="https://player.twitch.tv/?channel=pronewstv" height=" 600" width="900" allowfullscreen="true"></iframe>""",unsafe_allow_html=True)
-    
+    #st.markdown("""<iframe src="https://player.twitch.tv/?channel=pronewstv" height=" 600" width="900" allowfullscreen="true"></iframe>""",unsafe_allow_html=True)
+    st.markdown("""<iframe width="100%" height="100%" src="https://player.twitch.tv/?autoplay=false&channel=channel%3Dpronewstv&parent=share.streamlit.io" allow="accelerometer; autoplay; encrypted-media; picture-in-picture; fullscreen"  style="position: absolute; top: 0px; left: 0px; width: 100%; height: 100%;"></iframe>""",unsafe_allow_html=True)

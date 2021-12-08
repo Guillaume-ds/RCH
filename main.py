1919e741703080c5835fdd0bc61d9913b62ef540
Guillaume <guillaumedesurville@gmail.com>
2021-12-08 19:03:08 +0100

    favicon

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ from dateutil.relativedelta import relativedelta
 from page_direct import page_direct
 from page_stats import page_stats
 
-hr = "<hr style=' text-align : center; border-color : grey; margin-top: 10px; margin-bottom: 15px;'>"
-st.set_page_config(page_title="RCH",layout='wide',page_icon=":shark:")
 
+st.set_page_config(page_title="RCH",layout='wide',page_icon=":shark:")
+hr = "<hr style=' text-align : center; border-color : grey; margin-top: 10px; margin-bottom: 15px;'>"
 def main():
     #---------------------------------------------------------PAGES-------------------------------------------------------------------------------
     pages = {

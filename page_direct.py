2cbdfe696b6cf861497b80a251e6b54158b22e2a
Guillaume <guillaumedesurville@gmail.com>
2021-12-11 12:14:55 +0100

    venv

--- page_direct.py
+++ page_direct.py
@@ -1,9 +1,8 @@
 import streamlit as st
-import streamlit.components.v1 as components
 
 from streamlit_player import st_player
 
 def page_direct():
     
     st_player("https://www.twitch.tv/reservoirlive")          
-    st.markdown("""<iframe src="https://player.twitch.tv/?channel=reservoirlive&parent=share.streamlit.io"></iframe>""",unsafe_allow_html=True)
+    st.markdown("""<iframe src="https://player.twitch.tv/?channel=reservoirlive&parent=mighty-bastion-23461.herokuapp.com"></iframe>""",unsafe_allow_html=True)
